31ab3a6278aaea1ca5afec04cfa7156feeb23c7f
Sonia  Nicas <sduarte22@gmail.com>
2018-04-18 20:43:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,6 +109,15 @@ def winner
   end
 end  
 
-
+def play 
+  until over?
+  turn
+  end 
+  
+    if won?
+     puts "Congratulations #{winner}!"
+    elsif draw?
+     puts "Cat's Game!"
+ end 
 
 end 
