83526304de76bf2e58e0d2733417d05cc4c7b62f
Sonia  Nicas <sduarte22@gmail.com>
2018-04-18 20:14:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,13 +21,12 @@ WIN_COMBINATIONS = [
   puts " #{@board[3]} ""|"" #{@board[4]} ""|"" #{@board[5]} "
   puts "-----------"
   puts " #{@board[6]} ""|"" #{@board[7]} ""|"" #{@board[8]} "
-end 
+ end 
 
   def input_to_index(user_input)
   index user_input.to_i-1
- 
  return index 
-end
+ end
 
 
 end 
